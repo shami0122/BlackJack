c920b6ffb482deea38810cf1fd3f97067fa778bc
Shami Sharma <shamisharma01@outlook.com>
2022-09-28 19:33:42 -0400

    Create BlackJack.py

---
+++ BlackJack.py
@@ -0,0 +1,31 @@
+############### Blackjack Project #####################
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+logo = """
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
+
+print(logo)
+print("""---------------------------------------------------------------
+Welcome to the Game of BlackJack! 
+---------------------------------------------------------------""")
+start_game = input("If you want to start a new game, type 'y' or 'n to exit the game: ").upper()
+print(start_game)
