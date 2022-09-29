ae4fa1054f4d62349ea716eb1497c6ddc1e02f85
Shami Sharma <shamisharma01@outlook.com>
2022-09-28 20:02:14 -0400

    Update BlackJack.py
    Added Functions deal_cards(), calculate_score() and started on the game() function

--- BlackJack.py
+++ BlackJack.py
@@ -1,3 +1,5 @@
+import random
+
 ############### Blackjack Project #####################
 
 ############### Our Blackjack House Rules #####################
@@ -11,6 +13,31 @@
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
+
+
+
+def deal_cards():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
+
+def calculate_score():
+    return False
+
+
+def game():
+    player_cards = []
+    opponent_cards = []
+
+    for i in range(2):
+        player_cards.append(deal_cards)
+        opponent_cards.append(deal_cards)
+    
+    print(player_cards)
+    print(opponent_cards)
+
+
+
+
 
 logo = """
 .------.            _     _            _    _            _    
@@ -28,4 +55,8 @@ print("""---------------------------------------------------------------
 Welcome to the Game of BlackJack! 
 ---------------------------------------------------------------""")
 start_game = input("If you want to start a new game, type 'y' or 'n to exit the game: ").upper()
-print(start_game)
+if(start_game == "Y"):
+    game()
+else:
+    exit
+
